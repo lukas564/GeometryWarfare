a8e42452a732758d25c2d7ebd31d2bf4a1a07ee7
DESKTOP-M6GFLGV\palic <lpalicka6@gmail.com>
2017-12-27 20:58:41 +0100

    Unreal Engine starting point

---
+++ Source/GeometryWarfare/GeometryWarfareGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GeometryWarfareGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEOMETRYWARFARE_API AGeometryWarfareGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
